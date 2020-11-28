e0a09008d002449b3873f221e92f8d1034abb679
Evana Margain Puig <evanamp233@gmail.com>
2020-11-28 13:24:20 -0600

    Click handler for the nickname done button

--- app/src/main/java/com/evanamargain/aboutme/MainActivity.kt
+++ app/src/main/java/com/evanamargain/aboutme/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.evanamargain.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.evanamargain.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,20 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root
     setContentView(view)
+
+    binding.doneButton.setOnClickListener {
+      addNickname(it)
+    }
+  }
+
+  private fun addNickname(view: View) {
+    binding.nicknameText.text = binding.nicknameEdit.text
+    binding.nicknameEdit.visibility = View.GONE
+    view.visibility = View.GONE
+    binding.nicknameText.visibility = View.VISIBLE
+
+    // Hide the keyboard
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
   }
 }
